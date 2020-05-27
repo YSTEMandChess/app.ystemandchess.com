b737800df0206342080bdd47f7aa098befd16e5d
Benjamin <benjaminclark964@u.boisestate.edu>
2020-05-27 11:00:24 -0600

    added signup, and login page components

--- YStemAndChess/src/app/app-routing.module.ts
+++ YStemAndChess/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PlayComponent } from './pages/play/play.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 
 const routes: Routes = [
-  {path: '', component: PlayComponent}
+  {path: '', component: PlayComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent}
 ];
 
 @NgModule({
--- YStemAndChess/src/app/app.module.ts
+++ YStemAndChess/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayComponent } from './pages/play/play.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayComponent
+    PlayComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,

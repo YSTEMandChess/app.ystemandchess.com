edeb99f93cad996432284f8ec1656552e8f8e43d
Benjamin <benjaminclark964@u.boisestate.edu>
2020-05-28 14:29:02 -0600

    sign up verification and error messages

--- YStemAndChess/src/app/pages/signup/signup.component.ts
+++ YStemAndChess/src/app/pages/signup/signup.component.ts
@@ -7,9 +7,108 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  link = null;
+  private firstNameFlag = false;
+  private lastNameFlag = false;
+  private emailFlag = false;
+  private userNameFlag = false;
+  private passwordFlag = false;
+  private retypeFlag = false;
+  firstNameError = "";
+  lastNameError = "";
+  emailError = "";
+  userNameError = "";
+  passwordError = "";
+  retypePasswordError = "";
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  firstNameVerification() {
+    var firstName = (<HTMLInputElement>document.getElementById("firstName")).value;
+
+    if(firstName.length == 0) {
+      this.firstNameFlag = false;
+      this.firstNameError = "Invalid First Name"
+    } else {
+      this.firstNameFlag = true;
+      this.firstNameError = "";
+    }
+  }
+
+  lastNameVerification() {
+    var lastName = (<HTMLInputElement>document.getElementById("lastName")).value;
+
+    if(lastName.length == 0) {
+      this.lastNameFlag = false;
+      this.lastNameError = "Invalid Last Name"
+    } else {
+      this.lastNameFlag = true;
+      this.lastNameError = "";
+    }
+  }
+
+  emailVerification() {
+    var email = (<HTMLInputElement>document.getElementById("email")).value;
+    if(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}/.test(email)) {
+      this.emailFlag = true;
+      this.emailError = "";
+    } else {
+      this.emailFlag = false;
+      this.emailError = "Invalid Email"
+    }
+  }
+
+  usernameVerification() {
+    var username = (<HTMLInputElement>document.getElementById("username")).value;
+    
+    if(username.length < 2 || /[\^$.|?*+(){}]/.test("username")) {
+      //check username against database
+      this.userNameFlag= false;
+      this.userNameError = "Invalid Username";
+    } else {
+      this.userNameFlag= true;
+      this.userNameError = ""
+    }
+  }
+
+  passwordVerification() {
+    var password = (<HTMLInputElement>document.getElementById("password")).value;
+
+    if(password.length < 8) {
+      this.passwordFlag = false;
+      this.passwordError = "Invalid Password"
+    } else {
+      //verify password with username
+      this.passwordFlag = true;
+      this.passwordError = "";
+    }
+  }
+
+  retypePasswordVerification() {
+    var retypedPassword = (<HTMLInputElement>document.getElementById("retypedPassword")).value;
+    var password = (<HTMLInputElement>document.getElementById("password")).value;
+
+    if(retypedPassword === password) {
+      this.retypeFlag = true;
+      this.retypePasswordError = "";
+    } else {
+      this.retypeFlag = false;
+      this.retypePasswordError = "Passwords do not match"
+    }
+  }
+
+  checkIfValidAccount() {
+    if(this.firstNameFlag === true && this.lastNameFlag === true && this.emailFlag === true
+      && this.userNameFlag === true && this.passwordFlag === true && this.retypeFlag === true) {
+        this.link = "/login";
+      } else {
+        this.link = null;
+      }
+  }
+
+
 
 }

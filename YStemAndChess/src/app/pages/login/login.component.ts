b1d32bf0407f35ba76ce0be95f9a696b6902ed19
Benjamin <benjaminclark964@u.boisestate.edu>
2020-05-28 16:11:12 -0600

    login verification (without database check)

--- YStemAndChess/src/app/pages/login/login.component.ts
+++ YStemAndChess/src/app/pages/login/login.component.ts
@@ -7,9 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  link = null;
+  private usernameFlag = false;
+  private passwordFlag = false;
+  loginError = "";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  usernameVerification() {
+    var username = (<HTMLInputElement>document.getElementById("username")).value;
+
+    if(username.length > 2) {
+      //check username against database
+      this.usernameFlag = true;
+    } else {
+      this.usernameFlag = false;
+    }
+  }
+
+  passwordVerification() {
+    var password = (<HTMLInputElement>document.getElementById("password")).value;
+
+    if(password.length < 8) {
+      this.passwordFlag = false;
+    } else {
+      //check password against username in database
+      this.passwordFlag = true;
+    }
+  }
+
+  verifyUser() {
+    if(this.usernameFlag == true && this.passwordFlag == true) {
+      this.link = "";
+    } else {
+      this.link = "/login";
+    }
+  }
+
+  errorMessages() {
+    if(this.passwordFlag == false || this.usernameFlag == false) {
+      this.loginError = "Invalid username or password"
+    } else {
+      this.loginError = "";
+    }
   }
 
 }

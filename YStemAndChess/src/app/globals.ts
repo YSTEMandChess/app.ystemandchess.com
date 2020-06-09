83b4f4b7367917d7bcf5b9a671ff07716c51c65d
Owen Oertell <owen@oertell.net>
2020-06-09 15:13:31 -0400

    Adding logout and page direction

--- YStemAndChess/src/app/globals.ts
+++ YStemAndChess/src/app/globals.ts
@@ -5,13 +5,22 @@ var information;
 async function setPermissionLevel(cookie: CookieService) {
     let cookieName: string = "login";
     if (cookie.check(cookieName)) {
+        var rawData;
         let cookieContents: string = cookie.get(cookieName);
         let url = `http://localhost:8000/verify.php?jwt=${cookieContents}`;
-        await fetch(url).then(response => response.json()).then(data => information=data);
-        return information;
+        await fetch(url).then(response => response.text()).then(data => rawData = data.toString());
+        if(rawData.includes("Error: 405. This key has been tampered with or is out of date.")) {
+            cookie.delete(cookieName);
+            return {"error": "Error: 405: User authentication is not valid or expired"};
+        }  else {
+            information = JSON.parse(rawData);
+            return information;
+        }
     } else {
         console.log("User not logged in.");
+        return {"error": "User is not logged in"}
     }
+    
     
 }
 

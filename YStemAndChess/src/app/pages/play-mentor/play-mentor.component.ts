d8b786670073294451f96ca3d8431843a38e13c8
benjaminclark964 <46797974+benjaminclark964@users.noreply.github.com>
2020-06-16 13:24:44 -0600

    Play mentor (#19)
    * play-mentor init component
    
    * header/footer/style sheets
    
    * page header and statement
    
    * welcome statement
    
    * top of main container
    
    * bottom buttons
    
    * fixed button font
    
    * initialize lichess board
    
    * chess board iframe
    
    * blocked non mentors from accessing play-mentor page

---
+++ YStemAndChess/src/app/pages/play-mentor/play-mentor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-play-mentor',
+  templateUrl: './play-mentor.component.html',
+  styleUrls: ['./play-mentor.component.scss']
+})
+export class PlayMentorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

b86fdc4635e256caa8c039f44be64efbc4b6e89b
Owen Oertell <owen@oertell.net>
2020-06-18 16:31:33 -0400

    Adding Admin and Student Search Frontend

--- YStemAndChess/src/app/app-routing.module.ts
+++ YStemAndChess/src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
-import { PlayNologComponent } from './pages/play-nolog/play-nolog.component';
-import { ParentComponent } from './pages/parent/parent.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AdminComponent } from './pages/admin/admin.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PlayComponent } from './pages/play/play.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -11,6 +10,9 @@ import { BeAMentorComponent } from './pages/be-amentor/be-amentor.component';
 import { ProgramsComponent } from './pages/programs/programs.component';
 import { StudentComponent } from './pages/student/student.component';
 import { PlayMentorComponent} from './pages/play-mentor/play-mentor.component';
+import { PlayNologComponent } from './pages/play-nolog/play-nolog.component';
+import { ParentComponent } from './pages/parent/parent.component';
+
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -22,7 +24,8 @@ const routes: Routes = [
   {path: 'student', component: StudentComponent},
   {path: 'parent', component: ParentComponent},
   {path: 'play-mentor', component: PlayMentorComponent},
-  {path: 'play-nolog', component: PlayNologComponent}
+  {path: 'play-nolog', component: PlayNologComponent},
+  {path: 'admin', component: AdminComponent}
 ];
 
 @NgModule({

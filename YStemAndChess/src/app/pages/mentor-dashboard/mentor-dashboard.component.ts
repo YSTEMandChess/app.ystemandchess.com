11932966f49551e6b06f05a7c97a5189bbdd8ee6
Benjamin <benjaminclark964@u.boisestate.edu>
2020-06-18 14:46:01 -0600

    student stats static

--- YStemAndChess/src/app/pages/mentor-dashboard/mentor-dashboard.component.ts
+++ YStemAndChess/src/app/pages/mentor-dashboard/mentor-dashboard.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MentorDashboardComponent implements OnInit {
 
+  STUDENTS = [
+    {id: 1, name:'Superman'},
+    {id: 2, name:'Batman'},
+    {id: 5, name:'BatGirl'},
+    {id: 3, name:'Robin'},
+    {id: 4, name:'Flash'}
+];
   constructor() { }
 
   ngOnInit(): void {

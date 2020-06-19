c08e398a584ee02fd88c307bb7fd756b558c0811
Benjamin <benjaminclark964@u.boisestate.edu>
2020-06-19 09:53:16 -0600

    init

--- YStemAndChess/src/app/app.module.ts
+++ YStemAndChess/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { StudentComponent } from './pages/student/student.component';
 import { ParentComponent } from './pages/parent/parent.component';
 import { PlayMentorComponent } from './pages/play-mentor/play-mentor.component';
 import { PlayNologComponent } from './pages/play-nolog/play-nolog.component';
+import { DonateComponent } from './pages/donate/donate.component';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { PlayNologComponent } from './pages/play-nolog/play-nolog.component';
     StudentComponent,
     ParentComponent,
     PlayMentorComponent,
-    PlayNologComponent
+    PlayNologComponent,
+    DonateComponent
   ],
   imports: [
     BrowserModule,

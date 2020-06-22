0c239341508eaa45967e6068dc24c6a1a27c1078
Benjamin <benjaminclark964@u.boisestate.edu>
2020-06-22 16:47:24 -0600

    array of json objects

--- YStemAndChess/src/app/pages/signup/signup.component.ts
+++ YStemAndChess/src/app/pages/signup/signup.component.ts
@@ -237,8 +237,8 @@ export class SignupComponent implements OnInit {
     var studentPasssword = (<HTMLInputElement>document.getElementById("studentPassword")).value;
 
     let student = {first: studentFirstName, last: studentLastName, username: studentUserName, passsword: studentPasssword};
-
-    return student;
+    let obj = JSON.stringify(student);
+    return obj;
   }
 
   checkIfParent() {
@@ -301,6 +301,7 @@ export class SignupComponent implements OnInit {
     let url = "";
 
     if(accountType == 'parent' && this.newStudentFlag == true) {
+      
       url = `http://127.0.0.1:8000/?reason=create&first=${firstName}&last=${lastName}&email=${email}&password=${password}&username=${username}&role=${accountType}&students=${this.newStudents}`;
     } else {
       url = `http://127.0.0.1:8000/?reason=create&first=${firstName}&last=${lastName}&email=${email}&password=${password}&username=${username}&role=${accountType}&email=${email}`;

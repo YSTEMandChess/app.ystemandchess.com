113709687426d82566e57dbfa8fee6d89a02e5f8
benjaminclark964 <benjaminclark964@u.boisestate.edu>
2020-06-24 12:55:22 -0600

    name displays parent name

--- YStemAndChess/src/app/pages/parent/parent.component.ts
+++ YStemAndChess/src/app/pages/parent/parent.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { setPermissionLevel } from "../../globals";
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-parent',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
 
-  constructor() { }
+  username;
+  logged;
 
-  ngOnInit(): void {
+  constructor(private cookie: CookieService) { }
+
+  async ngOnInit() {
+    let pLevel = "nLogged";
+    let uInfo = await setPermissionLevel(this.cookie);
+    if (uInfo['error'] == undefined) {
+      this.logged = true;
+      pLevel = uInfo["role"];
+      this.username = uInfo["username"];
+    }
   }
 
 }

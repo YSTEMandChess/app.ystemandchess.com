dcdb8a3ec19dbc871d89134cd7bb4a2737bc5434
Owen Oertell <owen@oertell.net>
2020-07-03 22:06:10 -0400

    Adding chess board

--- YStemAndChess/src/app/pages/play-mentor/play-mentor.component.ts
+++ YStemAndChess/src/app/pages/play-mentor/play-mentor.component.ts
@@ -1,3 +1,5 @@
+import { CookieService } from 'ngx-cookie-service';
+import { SocketService } from './../../socket.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayMentorComponent implements OnInit {
 
-  constructor() { }
+  constructor(socket: SocketService, cookie: CookieService) { }
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void { 
+  }
+
+  public updateBoardState(data) {
+    let userContent = JSON.parse(atob(this.cookie.get("login").split(".")[1]));
+    console.log(`Sending an update: ${data}`);
+    this.socket.emitMessage("newState", JSON.stringify({boardState: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", username: userContent.username}));
   }
 
 }

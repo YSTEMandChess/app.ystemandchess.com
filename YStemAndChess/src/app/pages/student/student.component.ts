a35b03d647113a6849827e71e0274c450a8972ea
johiatt <jordanhiatt@u.boisestate.edu>
2020-07-11 14:46:10 -0600

    edit student component and html page to show user

--- YStemAndChess/src/app/pages/student/student.component.ts
+++ YStemAndChess/src/app/pages/student/student.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SocketService } from './../../socket.service';
+import { setPermissionLevel } from "../../globals";
 import { CookieService } from 'ngx-cookie-service';
 
 @Component({
@@ -8,11 +9,13 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./student.component.scss']
 })
 export class StudentComponent implements OnInit {
-
+  private logged: boolean;
+  username: string;
+  
   constructor(private socket: SocketService, private cookie: CookieService) { }
 
   ngOnInit(): void {
-    
+    this.getUsername();
   }
 
   public newGame() {
@@ -20,4 +23,19 @@ export class StudentComponent implements OnInit {
     this.socket.emitMessage("createNewGame", JSON.stringify({username: userContent.username}));
   }
 
+  public getName() {
+    console.log(this.username);
+    this.username;
+  }
+
+  private async getUsername() {
+    let pLevel = "nLogged";
+    let uInfo = await setPermissionLevel(this.cookie);
+    if (uInfo['error'] == undefined) {
+      this.logged = true;
+      pLevel = uInfo["role"];
+      this.username = uInfo["username"];
+      console.log(uInfo["username"]);
+    }
+  }
 }

948d544a0f8c5ee347768386723f09a39b3a9c48
benjaminclark964 <benjaminclark964@u.boisestate.edu>
2020-07-29 20:44:23 -0600

    removed students from mentor dashboard

--- YStemAndChess/src/app/pages/mentor-dashboard/mentor-dashboard.component.ts
+++ YStemAndChess/src/app/pages/mentor-dashboard/mentor-dashboard.component.ts
@@ -7,13 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MentorDashboardComponent implements OnInit {
 
-  STUDENTS = [
-    {id: 1, name:'Superman'},
-    {id: 2, name:'Batman'},
-    {id: 5, name:'BatGirl'},
-    {id: 3, name:'Robin'},
-    {id: 4, name:'Flash'}
-];
   constructor() { }
 
   ngOnInit(): void {

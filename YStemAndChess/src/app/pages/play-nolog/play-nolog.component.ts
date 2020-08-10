f4565a1271aeff5d63f9cd45b965e89acc2e91fd
Owen Oertell <owen@oertell.net>
2020-08-09 22:04:59 -0400

    Adding better chess engine v2

--- YStemAndChess/src/app/pages/play-nolog/play-nolog.component.ts
+++ YStemAndChess/src/app/pages/play-nolog/play-nolog.component.ts
@@ -1,3 +1,4 @@
+import { createAotUrlResolver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,12 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./play-nolog.component.scss']
 })
 export class PlayNologComponent implements OnInit {
-  
+
   private messageQueue = new Array();
   private isReady: boolean;
-  private color:String;
-  private level:number = 3;
-  private currentFEN:String = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+  private color: String = "white";
+  private level: number = 5;
+  private currentFEN: String = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
   constructor() { }
 
@@ -19,7 +20,7 @@ export class PlayNologComponent implements OnInit {
     var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
     var eventer = window[eventMethod];
     var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
-    
+
     this.newGameInit();
 
     // Listen to message from child window
@@ -29,23 +30,26 @@ export class PlayNologComponent implements OnInit {
         console.log("this does work every time");
         let info = e.data;
         console.log("I am info " + info);
-        if(info == "ReadyToRecieve") {
-          this.isReady=true;
+        if (info == "ReadyToRecieve") {
+          this.isReady = true;
           this.sendFromQueue();
-        } else if(info == "checkmate") {
+        } else if (info == "checkmate") {
           this.gameOverAlert();
-        } else if(info == "draw") {
+        } else if (info == "draw") {
           this.gameOverAlert();
-        } else if(info == "gameOver") {
+        } else if (info == "gameOver") {
           this.gameOverAlert();
         } else {
           this.currentFEN = info;
+          this.level = parseInt((<HTMLInputElement>document.getElementById('movesAhead')).value);
+          if(this.level <= 1) this.level = 1;
+          else if (this.level >= 30) this.level = 30;
           this.httpGetAsync(`http://127.0.0.1:8080/?level=${this.level}&fen=${this.currentFEN}`, (response) => {
             if (this.isReady) {
-              console.log("sending message "+ JSON.stringify({ boardState: response, color: this.color }));
+              console.log("sending message " + JSON.stringify({ boardState: response, color: this.color }));
               var chessBoard = (<HTMLFrameElement>document.getElementById('chessBd')).contentWindow;
               chessBoard.postMessage(JSON.stringify({ boardState: response, color: this.color }), "http://localhost");
-            } else  {
+            } else {
               this.messageQueue.push(JSON.stringify({ boardState: response, color: this.color }));
             }
           });
@@ -53,24 +57,38 @@ export class PlayNologComponent implements OnInit {
       }
     }, false);
   }
-  
+
   private sendFromQueue() {
     this.messageQueue.forEach(element => {
-      console.log("sending message " +element);
+      console.log("sending message " + element);
       var chessBoard = (<HTMLFrameElement>document.getElementById('chessBd')).contentWindow;
       chessBoard.postMessage(element, "http://localhost");
     });
   }
 
   public newGameInit() {
-    this.color = (Math.random() > .5) ? "white" : "black";
+    console.log("A new game has been requested")
+    this.color = (Math.random() > 0.5) ? "white" : "black";
+    this.currentFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    
+    if (this.isReady) {
+      console.log("sending message" + this.currentFEN);
+      var chessBoard = (<HTMLFrameElement>document.getElementById('chessBd')).contentWindow;
+      chessBoard.postMessage(JSON.stringify({ boardState: this.currentFEN, color: this.color }), "http://localhost");
+    } else {
+      this.messageQueue.push(JSON.stringify({ boardState: this.currentFEN, color: this.color }));
+    }
+    
     if (this.color == "black") {
+      this.level = parseInt((<HTMLInputElement>document.getElementById('movesAhead')).value);
+      if(this.level <= 1) this.level = 1;
+      else if (this.level >= 30) this.level = 30;
       this.httpGetAsync(`http://127.0.0.1:8080/?level=${this.level}&fen=${this.currentFEN}`, (response) => {
         if (this.isReady) {
           console.log("sending message" + response);
           var chessBoard = (<HTMLFrameElement>document.getElementById('chessBd')).contentWindow;
           chessBoard.postMessage(JSON.stringify({ boardState: response, color: this.color }), "http://localhost");
-        } else  {
+        } else {
           this.messageQueue.push(JSON.stringify({ boardState: response, color: this.color }));
         }
       });

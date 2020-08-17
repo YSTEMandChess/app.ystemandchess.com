32aa2498a9d65e9b0006b0792ab3d13e939d1502
benjaminclark964 <benjaminclark964@u.boisestate.edu>
2020-08-17 11:28:44 -0600

    fixed url bug

--- YStemAndChess/src/app/pages/pieceLessons/pawn-lessons/pawn-lessons.component.ts
+++ YStemAndChess/src/app/pages/pieceLessons/pawn-lessons/pawn-lessons.component.ts
@@ -20,7 +20,7 @@ export class PawnLessonsComponent implements OnInit {
   }
 
   private getLessonsCompleted() {
-    let url = `http://127.0.0.1/getCompletedLesson.php/?jwt=${this.cookie.get('login')}&piece=pawn`;
+    let url = `http://127.0.0.1:8000/getCompletedLesson.php/?jwt=${this.cookie.get('login')}&piece=pawn`;
     this.httpGetAsync(url, (response) => {
       let data = JSON.parse(response);
       this.lessonNum = data["lessonNumber"];
@@ -28,12 +28,12 @@ export class PawnLessonsComponent implements OnInit {
   }
 
   private getCurrentLesson() {
-    let url = `http://127.0.0.1/getLesson.php/?jwt=${this.cookie.get('login')}&piece=pawn`;
+    let url = `http://127.0.0.1:8000/getLesson.php/?jwt=${this.cookie.get('login')}&piece=pawn`;
     this.httpGetAsync(url, (response) => {
       let data = JSON.parse(response);
       this.lessonStartFEN = data['startFen'];
       this.lessonEndFEN = data['endFen'];
-    })
+    });
   }
 
   private httpGetAsync(theUrl: string, callback) {

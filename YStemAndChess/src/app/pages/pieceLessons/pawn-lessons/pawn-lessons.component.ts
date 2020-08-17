70ea20e113e7f3ace11189f9584f0fa48ab8c1c7
benjaminclark964 <benjaminclark964@u.boisestate.edu>
2020-08-17 17:16:54 -0600

    stockfish now works with lessons

--- YStemAndChess/src/app/pages/pieceLessons/pawn-lessons/pawn-lessons.component.ts
+++ YStemAndChess/src/app/pages/pieceLessons/pawn-lessons/pawn-lessons.component.ts
@@ -13,6 +13,12 @@ export class PawnLessonsComponent implements OnInit {
   private lessonStarted: boolean = false;
   private endSquare: string = "";
   private lessonNum = "";
+  private boardState = "";
+  private currentFEN = "";
+  private messageQueue = new Array();
+  private color = "white";
+  private level = 5;
+  private isReady = false;
   public displayLessonNum = 0;
 
   constructor(private cookie: CookieService) { }
@@ -26,13 +32,32 @@ export class PawnLessonsComponent implements OnInit {
     eventer(messageEvent, (e) => {
       console.log("the end FEN " + this.lessonEndFEN);
       if (e.origin == "http://localhost") {
+        if(e.data == "ReadyToRecieve") {
+          this.isReady = true;
+          this.sendFromQueue();
+        }
         if(this.lessonStarted == false) {
           this.getLessonsCompleted()
           this.getCurrentLesson();
           this.lessonStarted = true;
         } 
-        if(e.data == this.lessonEndFEN) {
+        else if(e.data == this.lessonEndFEN) {
           alert("Lesson " + this.displayLessonNum + " completed!");
+        } else {
+          this.currentFEN = e.data;
+          this.level = 5;
+          if(this.level <= 1) this.level = 1;
+          else if (this.level >= 30) this.level = 30;
+          console.log(e.data)
+          this.httpGetAsync(`http://127.0.0.1:8080/?level=${this.level}&fen=${this.currentFEN}`, (response) => {
+            if (this.isReady) {
+              console.log("sending message " + JSON.stringify({ boardState: response, color: this.color }));
+              var chessBoard = (<HTMLFrameElement>document.getElementById('chessBd')).contentWindow;
+              chessBoard.postMessage(JSON.stringify({ boardState: response, color: this.color }), "http://localhost");
+            } else {
+              this.messageQueue.push(JSON.stringify({ boardState: response, color: this.color }));
+            }
+          });
         }
       }
     }, false);
@@ -58,11 +83,48 @@ export class PawnLessonsComponent implements OnInit {
     });
   }
 
+  private sendFromQueue() {
+    this.messageQueue.forEach(element => {
+      console.log("sending message " + element);
+      var chessBoard = (<HTMLFrameElement>document.getElementById('chessBd')).contentWindow;
+      chessBoard.postMessage(element, "http://localhost");
+    });
+  }
+
+  public newGameInit() {
+    console.log("A new game has been requested")
+    this.currentFEN = this.lessonStartFEN;
+    
+    if (this.isReady) {
+      console.log("sending message" + this.currentFEN);
+      var chessBoard = (<HTMLFrameElement>document.getElementById('chessBd')).contentWindow;
+      chessBoard.postMessage(JSON.stringify({ boardState: this.currentFEN, color: this.color }), "http://localhost");
+    } else {
+      this.messageQueue.push(JSON.stringify({ boardState: this.currentFEN, color: this.color }));
+    }
+    
+    if (this.color == "white") {
+      this.level = 5;
+      this.color = "black";
+      if(this.level <= 1) this.level = 1;
+      else if (this.level >= 30) this.level = 30;
+      this.httpGetAsync(`http://127.0.0.1:8080/?level=${this.level}&fen=${this.currentFEN}`, (response) => {
+        if (this.isReady) {
+          console.log("sending message" + response);
+          var chessBoard = (<HTMLFrameElement>document.getElementById('chessBd')).contentWindow;
+          chessBoard.postMessage(JSON.stringify({ boardState: response, color: this.color }), "http://localhost");
+        } else {
+          this.messageQueue.push(JSON.stringify({ boardState: response, color: this.color }));
+        }
+      });
+    } 
+  }
+
   private sendLessonToChessBoard() {
     var chessBoard = (<HTMLFrameElement>document.getElementById('chessBd')).contentWindow;
     console.log("start " + this.lessonStartFEN);
     console.log("end " + this.lessonEndFEN);
-    chessBoard.postMessage(JSON.stringify({ boardState: this.lessonStartFEN, endState: this.lessonEndFEN, lessonFlag: true, endSquare: this.endSquare }), "http://localhost");
+    chessBoard.postMessage(JSON.stringify({ boardState: this.lessonStartFEN, endState: this.lessonEndFEN, lessonFlag: true, endSquare: this.endSquare, color: this.color }), "http://localhost");
   }
 
   private httpGetAsync(theUrl: string, callback) {

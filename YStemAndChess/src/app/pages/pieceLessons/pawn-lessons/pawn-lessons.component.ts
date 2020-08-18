5a8ce9c43584971e81bb995f5d8af4186d459b80
benjaminclark964 <benjaminclark964@u.boisestate.edu>
2020-08-18 12:23:04 -0600

    lessons now load one after the other

--- YStemAndChess/src/app/pages/pieceLessons/pawn-lessons/pawn-lessons.component.ts
+++ YStemAndChess/src/app/pages/pieceLessons/pawn-lessons/pawn-lessons.component.ts
@@ -13,11 +13,10 @@ export class PawnLessonsComponent implements OnInit {
   private lessonStarted: boolean = false;
   private endSquare: string = "";
   private lessonNum = "";
-  private boardState = "";
   private currentFEN = "";
   private messageQueue = new Array();
   private color = "white";
-  private level = 5;
+  private level = 5; //default stockfish value
   private isReady = false;
   public displayLessonNum = 0;
 
@@ -30,29 +29,31 @@ export class PawnLessonsComponent implements OnInit {
 
     // Listen to message from child window
     eventer(messageEvent, async (e) => {
-      console.log("the end FEN " + this.lessonEndFEN);
       if (e.origin == "http://localhost") {
+        //child window has loaded and can now recieve data
         if(e.data == "ReadyToRecieve") {
           this.isReady = true;
           this.sendFromQueue();
         }
+        //gets the amount of lessons completed if not in lesson
         if(this.lessonStarted == false) {
           await this.getLessonsCompleted()
           this.lessonStarted = true;
         } 
+        //if lesson is completed start new lesson
         else if(e.data == this.lessonEndFEN) {
           this.updateLessonCompletion();
           alert("Lesson " + this.displayLessonNum + " completed!");
           await this.getLessonsCompleted();
+          window.location.reload();
         } else {
+          //stockfish move
           this.currentFEN = e.data;
           this.level = 5;
           if(this.level <= 1) this.level = 1;
           else if (this.level >= 30) this.level = 30;
-          console.log(e.data)
           this.httpGetAsync(`http://127.0.0.1:8080/?level=${this.level}&fen=${this.currentFEN}`, (response) => {
             if (this.isReady) {
-              console.log("sending message " + JSON.stringify({ boardState: response, color: this.color }));
               var chessBoard = (<HTMLFrameElement>document.getElementById('chessBd')).contentWindow;
               chessBoard.postMessage(JSON.stringify({ boardState: response, color: this.color }), "http://localhost");
             } else {
@@ -64,6 +65,9 @@ export class PawnLessonsComponent implements OnInit {
     }, false);
   }
 
+  /**
+   * get the last lesson completed by the student for the pawn
+   */
   private async getLessonsCompleted() {
     let url = `http://127.0.0.1:8000/getCompletedLesson.php/?jwt=${this.cookie.get('login')}&piece=pawn`;
       this.httpGetAsync(url, (response) => {
@@ -73,6 +77,9 @@ export class PawnLessonsComponent implements OnInit {
     });
   }
 
+  /**
+   * get current lesson data
+   */
   private async getCurrentLesson() {
     let url = `http://127.0.0.1:8000/getLesson.php/?jwt=${this.cookie.get('login')}&piece=pawn&lessonNumber=${this.lessonNum}`;
     console.log("I am lesson Num " + this.lessonNum);
@@ -81,12 +88,28 @@ export class PawnLessonsComponent implements OnInit {
       this.lessonStartFEN = data['startFen'];
       this.lessonEndFEN = data['endFen'];
       this.displayLessonNum = data['lessonNumber'];
-      console.log("I am display Lesson " + this.displayLessonNum)
+      if(this.checkIfLessonsAreCompleted() === true) { return; }
       this.endSquare = data['endSquare'];
       this.sendLessonToChessBoard();
     });
   }
 
+  /**
+   * checks if all lessons for the pawn have been completed
+   */
+  private checkIfLessonsAreCompleted(): boolean {
+    let completed: boolean = false;
+    if(this.displayLessonNum === undefined) {
+      alert("Congratulations all current lessons for this piece have been completed!" + '\n' +
+        "Comeback soon for more lessons or go over previous lessons.");
+        completed = true;
+    }
+    return completed;
+  }
+
+  /**
+   * store data for chess board before it has loaded
+   */
   private sendFromQueue() {
     this.messageQueue.forEach(element => {
       console.log("sending message " + element);
@@ -95,6 +118,9 @@ export class PawnLessonsComponent implements OnInit {
     });
   }
 
+  /**
+   * initialize stockfish
+   */
   public newGameInit() {
     console.log("A new game has been requested")
     this.currentFEN = this.lessonStartFEN;
@@ -124,6 +150,9 @@ export class PawnLessonsComponent implements OnInit {
     } 
   }
 
+  /**
+   * send current lesson to chess board
+   */
   private sendLessonToChessBoard() {
     var chessBoard = (<HTMLFrameElement>document.getElementById('chessBd')).contentWindow;
     console.log("start " + this.lessonStartFEN);
@@ -131,6 +160,13 @@ export class PawnLessonsComponent implements OnInit {
     chessBoard.postMessage(JSON.stringify({ boardState: this.lessonStartFEN, endState: this.lessonEndFEN, lessonFlag: true, endSquare: this.endSquare, color: this.color }), "http://localhost");
   }
 
+  public previousLesson() {
+    
+  }
+
+  /**
+   * update student lesson completion for database
+   */
   private async updateLessonCompletion() {
     let url = `http://127.0.0.1:8000/updateLessonCompletion.php/?jwt=${this.cookie.get("login")}&piece=pawn&lessonNumber=${this.lessonNum}`;
     this.httpGetAsync(url, (response) => {
@@ -138,10 +174,11 @@ export class PawnLessonsComponent implements OnInit {
     });
   }
 
-  private nextLesson() {
-    this.ngOnInit();
-  }
-
+  /**
+   * make requests to the serer
+   * @param theUrl url to contact
+   * @param callback response
+   */
   private httpGetAsync(theUrl: string, callback) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function () {

2880c562914cdba5f7e1f9724f7d4367ef28a8e3
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-11-25 16:45:10 -0800

    added env files and added donate-box widget as well as resized mentor-statement

---
+++ YStemAndChess/environments/environment.prod.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    agora: {
+      appId: '6c368b93b82a4b3e9fb8e57da830f2a4',
+    },
+    urls: {
+      middlewareURL: 'http://127.0.0.1:8000',
+      chessClientURL: 'http://localhost',
+      stockFishURL : 'http://127.0.0.1:8080',
+      chessServer : 'http://localhost:3000'
+    }
+  };
+  
+  /*
+   * For easier debugging in development mode, you can import the following file
+   * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+   *
+   * This import should be commented out in production mode because it will have a negative impact
+   * on performance if an error is thrown.
+   */
+  // import 'zone.js/dist/zone-error';  // Included with Angular CLI.

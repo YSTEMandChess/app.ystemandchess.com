a003ede19f5f03f5fce333163e3b9bd031f6b709
Darien Cheng <cheng.darien@gmail.com>
2021-02-09 15:02:11 -0500

    Finish movelist feature

--- YStemAndChess/src/app/pages/board-analyzer/board-analyzer.component.ts
+++ YStemAndChess/src/app/pages/board-analyzer/board-analyzer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Move, MoveIndexes } from './board-analyzer.model';
 import { environment } from 'src/environments/environment';
 
 declare var ChessBoard:any;
@@ -9,18 +10,21 @@ declare var Chess:any;
   templateUrl: './board-analyzer.component.html',
   styleUrls: ['./board-analyzer.component.scss']
 })
+
 export class BoardAnalyzerComponent implements OnInit {
 
   private board;
   private chess;
-  private useAnimation: boolean = false;
   private level: number = 10;
   private centipawn: string = "0";
   private principleVariation: Array<String>;
   private prevFEN: String =
     'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
-  private turn: number = 1;
-  private color: String = "white";
+
+  // Plies is an array of Moves
+  // Moves contain up to two Move Objects
+  public plies: Array<Array<Move>> = [];
+  public currMove: Move    // Current move one the board
 
   //debounce functions
   public depthDebounce = this.debounce(this.onDepthInput.bind(this));
@@ -66,16 +70,14 @@ export class BoardAnalyzerComponent implements OnInit {
   //Obtian the centipawn value and principle variation form
   //the stockfish server response
   private parseStockfish(str: string) {
-    var res: Array<string> = JSON.parse(str);
-    console.log(res[res.length -2]);
-    
+    var res: Array<string> = JSON.parse(str);    
     var split = res[res.length -2].split(" ");
 
     if (split.indexOf("mate") != -1) {
       this.centipawn = "M" + parseInt(split[split.indexOf("mate") + 1]);
     } else {
         var value: number = parseInt(split[split.indexOf("cp") + 1])/100;
-        value = this.color == "black" ? value * -1 : value;
+        value = this.chess.turn() == "b" ? value * -1 : value;
         var sign: string = value < 0 ? "" : "+";
 
         this.centipawn = sign + value;
@@ -99,7 +101,7 @@ export class BoardAnalyzerComponent implements OnInit {
     var i = 0;
     var j = 1;
 
-    if (this.color == "black") {
+    if (this.chess.turn() == "b") {
       formatPV = "1... " + this.principleVariation[0] + " ";
       i = 1;
       j = 2;
@@ -128,6 +130,8 @@ export class BoardAnalyzerComponent implements OnInit {
     // illegal move
     if (move === null) return 'snapback';
 
+    this.updateMoveList(move);
+    
     this.httpGetAsync(
       `${environment.urls.stockFishURL}/?info=${"true"}&level=${this.level}&fen=${this.prevFEN}&move=${source+target}`,
       (response) => {
@@ -137,7 +141,6 @@ export class BoardAnalyzerComponent implements OnInit {
     );
 
     this.prevFEN = this.chess.fen();
-    this.color = this.color == "white" ? "black" : "white";
   }
 
   private onSnapEnd() {
@@ -174,5 +177,94 @@ export class BoardAnalyzerComponent implements OnInit {
     }
 
     this.updateEngineEvaluation();
+  }
+
+  //@move-Move object returned by chess.js move method
+  //Add a move object consisting of the move in SAN and the FEN
+  //Add move to curr ply or create a new ply
+  private updateMoveList(move) {
+    var currPly = this.plies[this.plies.length - 1]
+    var movesPerPly = 2;
+    var moveObj: Move = {
+      move: move.san,
+      indexes: !currPly ? { pIndex: 0, mIndex: 0 } : this.getNextMoveIndex(this.currMove.indexes),
+      fen: this.chess.fen()
+    }
+
+    if (!currPly) {
+      this.plies.push([moveObj]);
+      this.currMove = moveObj;
+      return;
+    }
+
+
+    // If the new move would overwrite an existing move
+    // then delete all moves from the existing move onward
+    // unless the new move is the same as the existing move
+    if (this.currMove != currPly[currPly.length - 1]) {
+      var nextMoveIndexes = this.getNextMoveIndex(this.currMove.indexes);
+      var nextMove = this.plies[nextMoveIndexes.pIndex][nextMoveIndexes.mIndex];
+
+      if(JSON.stringify(nextMove) === JSON.stringify(moveObj)) {
+        this.currMove = nextMove;
+        return;
+      }
+
+      for (var i = this.plies.length - 1; i >= 0; i--) {
+        var found: boolean = false;
+        for (var j = 1; j >= 0; j--) {
+          if (this.plies[i][j] != this.currMove) {
+            this.plies[i].pop();
+          } else {
+            found = true;
+            currPly = this.plies[this.plies.length - 1];
+            break;
+          }
+        }
+        if (found) break;
+        this.plies.pop();
+      }
+    }
+
+    this.currMove = moveObj;
+
+    if (!currPly[movesPerPly - 1]) {
+      this.plies[this.plies.length - 1].push(moveObj);
+      return
+    }
+
+    this.plies.push([moveObj]);
+  }
+
+  private getLastMoveIndexes(): MoveIndexes {
+    var numPly = this.plies.length - 1;
+    var moveObj: MoveIndexes = {
+      pIndex: numPly,
+      mIndex: this.plies[numPly].length - 1
+    }
+    return moveObj;
+  }
+
+  private getNextMoveIndex(moveIndexes: MoveIndexes): MoveIndexes {
+    var nextMoveIndexes: MoveIndexes = {
+      pIndex: moveIndexes.pIndex,
+      mIndex: moveIndexes.mIndex
+    };
+    if (moveIndexes.mIndex == 0) {
+      nextMoveIndexes.mIndex++;
+    } else {
+      nextMoveIndexes.pIndex++;
+      nextMoveIndexes.mIndex = 0;
+    }
+    return nextMoveIndexes;
+  }
+
+
+  //@move-Movelist move object
+  public setMove(move) {
+    this.chess.load(move.fen);
+    this.board.position(move.fen);
+    this.currMove = move;
+    this.updateEngineEvaluation();    
   }
 }

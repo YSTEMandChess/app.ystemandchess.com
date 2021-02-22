cda002bb65cc1722ff140169519c7e58d8b0303c
Alex <alexhuah@gmail.com>
2021-02-22 14:37:57 -0800

    update: made changes to start api server and made sure it works

---
+++ middlewareNode/config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const config = require('config')
+const db = config.get('mongoURI')
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    console.log('MongoDB Connected...')
+  } catch (err) {
+    console.error(err.message)
+    process.exit(1) // Exit process if connection fails
+  }
+}
+
+module.exports = connectDB
---
+++ middlewareNode/config/passport.js
@@ -0,0 +1,35 @@
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+const passport = require('passport')
+const config = require('config')
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id)
+})
+
+passport.deserializeUser(function (id, done) {
+  //User.findById(id, function (err, user) {
+  done(err, user)
+  //})
+})
+
+passport.use(
+  new JwtStrategy(
+    {
+      secretOrKey: config.get('jwtSecret'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    },
+    async (jwt_payload, done) => {
+      try {
+        //const user = await User.findById(jwt_payload.user.id)
+        if (user) {
+          return done(null, user)
+        } else {
+          return done(null, false)
+        }
+      } catch (err) {
+        return done(err, false)
+      }
+    }
+  )
+)

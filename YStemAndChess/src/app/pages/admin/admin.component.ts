02d7c77ed3b257fb36531581dee60231efb51645
Bryan Valencia <bryanlikescode@gmail.com>
2021-07-06 18:47:18 -0500

    Created two routes for the admin to search for students and connected it to the admin page

--- YStemAndChess/src/app/pages/admin/admin.component.ts
+++ YStemAndChess/src/app/pages/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-admin',
@@ -10,6 +11,78 @@ export class AdminComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  searchStudent() {
+    console.log("its working");
+    // Retrieve name from input field and split by space
+    var fullName = (<HTMLInputElement>document.getElementById('name')).value;
+    var nameArray = fullName.split(" ");
+    // Assert only first name has been entered
+    if (nameArray.length == 1) {
+      // First name is the first and only entry in the nameArray
+      let firstName = nameArray[0];
+      // Note the route findUsers takes only the first name
+      let url = `${environment.urls.middlewareURL}/users/selectByFirstName?first=${firstName}`;
+      this.httpGetAsync(url, (response) => {
+        if (response == 'No users were found.') {
+          document.getElementById("studentProgressTable").innerHTML += '<br> <p>No users were found.</p>';
+        } else {
+          // Build HTML string 
+          let html = '';
+          for (let i = 0; i < response.length() ; i++){
+            html += '<br>' // break line
+            html += '<tr>'
+            html += '<td>'+response[i]+'</td>'
+            html += '<td>Placeholder</td>'
+            html += '<td>Data</td>'
+            html += '</tr>'
+          }
+      
+          // Populate HTML with the html string built
+          document.getElementById("studentProgressTable").innerHTML += html;
+
+        }
+      });
+    }
+    // Assert first and last name have been entered
+    else {
+      // First name is the first entry and last name the second entry in the nameArray
+      let firstName = nameArray[0];
+      let lastName = nameArray[1];
+      // Note the route findUser takes both first and last name
+      let url = `${environment.urls.middlewareURL}/users/selectByFullName?first=${firstName}&last=${lastName}`;
+      this.httpGetAsync(url, (response) => {
+        if (response == 'No users were found.') {
+          alert('No users were found.');
+        } else {
+          // Build HTML string 
+          let html = '';
+          for (let i = 0; i < response.length() ; i++){
+            html += '<br>' // break line
+            html += '<tr>'
+            html += '<td>'+response[i]+'</td>'
+            html += '<td>Placeholder</td>'
+            html += '<td>Data</td>'
+            html += '</tr>'
+          }
+      
+          // Populate HTML with the html string built
+          document.getElementById("studentProgressTable").innerHTML += html;
+
+        }
+      });
+    }
+  }
+
+  private httpGetAsync(theUrl: string, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        callback(xmlHttp.responseText);
+    };
+    xmlHttp.open('POST', theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
   }
 
 }
--- middlewareNode/routes/users.js
+++ middlewareNode/routes/users.js
@@ -175,4 +175,93 @@ router.post(
   }
 )
 
+// @route   POST /users/selectByFirstName
+// @desc    POST return the users corresponding to the first name queried
+// @access  Public
+router.post(
+  '/selectByFirstName',
+  [
+    check('first', 'First name is required').not().isEmpty()
+  ],
+  async (req, res) => {
+    try {
+      /* Not sure if this is necessary for this route
+      //Validation checks to ensure the required fields are present
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+      }
+      */
+      const { first } = req.query
+
+      //Find all users
+      let foundUsers = await users.find()
+      let returnArray = [];
+
+      // Find all users with a corresponding first name 
+      for (let i = 0; i < foundUsers.length() ; i++) {
+        if (foundUsers[i].firstName.includes(first)) {
+          let fullName = foundUsers[i].firstName+' '+foundUsers[i].lastName;
+          returnArray.push(fullName);
+        };
+      }
+
+      // If returnArray is empty, no users were found
+      if ( returnArray.isEmpty() ) {
+        return res.status(204).json('No users were found.')
+      } else {
+        return returnArray;
+      }
+    } catch (error) {
+      console.error(error.message)
+      res.status(500).send('Server error')
+    }
+  }
+)
+
+// @route   POST /users/selectByFullName
+// @desc    POST return the users corresponding to the first and last name queried
+// @access  Public
+router.post(
+  '/selectByFullName',
+  [
+    check('first', 'First name is required').not().isEmpty(),
+    check('last', 'Last name is required').not().isEmpty()
+  ],
+  async (req, res) => {
+    try {
+      /* Not sure if this is necessary for this route
+      //Validation checks to ensure the required fields are present
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+      }
+      */
+      const { first , last } = req.query
+
+      //Find all users
+      let foundUsers = await users.find({ first })
+      let returnArray = [];
+
+      // Find all users with a corresponding first name 
+      for (let i = 0; i < foundUsers.length() ; i++) {
+        if (foundUsers[i].lastName.includes(last)) {
+          let fullName = foundUsers[i].firstName+' '+foundUsers[i].lastName;
+          returnArray.push(fullName);
+        };
+      }
+
+      // If returnArray is empty, no users were found
+      if ( returnArray.isEmpty() ) {
+        return res.status(204).json('No users were found.')
+      } else {
+        return returnArray;
+      }
+    } catch (error) {
+      console.error(error.message)
+      res.status(500).send('Server error')
+    }
+  }
+)
+
 module.exports = router

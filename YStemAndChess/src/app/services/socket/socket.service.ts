cd5ac9eb5293f1fdf630d89906b994e71f665d83
Khyati Vadher <khyati.vadher@edeltaes.com>
2022-11-24 15:46:03 +0530

    added console

--- YStemAndChess/src/app/services/socket/socket.service.ts
+++ YStemAndChess/src/app/services/socket/socket.service.ts
@@ -12,15 +12,21 @@ export class SocketService {
     this.socket = io.connect(environment.urls.chessServer, {
       transports: ['websocket'],
     });
+    console.log('this.socket : ', this.socket);
   }
 
   public emitMessage(eventName: string, message: string) {
+    console.log('this.socket: ', this.socket);
     this.socket.emit(eventName, message);
+    console.log('this.socket', this.socket);
   }
 
   public listen(eventName: string) {
     return new Observable((subscriber) => {
+      console.log('subscriber: ', subscriber);
       this.socket.on(eventName, (data) => {
+        console.log('data: ', data);
+        console.log('this.socket: ', this.socket);
         subscriber.next(data);
       });
     });

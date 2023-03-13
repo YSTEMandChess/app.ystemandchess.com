2da040d068e906ef38ad9d31214b6a15ebd88ad2
Inaya Alkhatib <70734277+inayaAAAA@users.noreply.github.com>
2023-03-13 18:10:47 -0400

    Added POST and PUT requests for timetracking event

--- middlewareNode/routes/users.js
+++ middlewareNode/routes/users.js
@@ -34,6 +34,54 @@ router.get("/children", passport.authenticate("jwt"), async (req, res) => {
   }
 });
 
+// @route   POST /user/timeTracking/start
+// @desc    POST a user's event for time tracking
+// @access  Public with jwt Authentication
+router.post("/timeTracking/start", passport.authenticate("jwt"), async (req, res) => {
+  try{
+    const {username, eventType} = req.query
+    const eventId = uuidv4(); //Generate a random meetingId
+
+    // Creating an event with required fields
+    await timeTracking.create({
+      username: username, 
+      eventType: eventType,
+      eventId: eventId,
+      startTime: new Date(),
+    });
+
+    return res.status(200).json("Ok");
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json("Server error");
+  }
+});
+
+// @route   PUT /user/timeTracking/end
+// @desc    PUT a user's event to an end
+// @access  Public with jwt Authentication
+router.put("/timeTracking/end", passport.authenticate("jwt"), async (req, res) => {
+  try{
+    const {username, eventType, eventId} = req.query
+    let filters = {username: username, eventID: eventId, eventType: eventType}
+    const currEvent = await timeTracking.findOne(filters);
+
+    //Error checking to ensure the user is actually in a event
+    if (!currEvent){
+      return res.status(400).json("This event is currently not happening!")
+    }
+
+    // Saving end time 
+    currEvent.endTime = new Date();
+    await currEvent.save();
+
+    return res.status(200).json("Ok");
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json("Server error");
+  }
+});
+
 // @route   POST /user/
 // @desc    POST Signup the requested user with the provided credentials
 // @access  Public

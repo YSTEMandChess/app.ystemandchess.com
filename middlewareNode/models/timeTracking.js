4cddd9a7f857f91673b74970d636fb61cccae07c
Inaya Alkhatib <inayahill@gmail.com>
2023-03-23 19:12:50 -0400

    Minor comment added
    Signed-off-by: Inaya Alkhatib <inayahill@gmail.com>

--- middlewareNode/models/timeTracking.js
+++ middlewareNode/models/timeTracking.js
@@ -24,7 +24,7 @@ const timeTrackingSchema = new Schema(
             type: Date
         },
         totalTime: {
-            type: Number
+            type: Number // Seconds
         }
     },
     { versionKey: false }

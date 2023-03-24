c9e45ef0a184a48df99b1eff922e2620c4555e16
Samantha Zheng <samanthaz@vt.edu>
2023-03-24 15:04:54 -0400

    added student-profile

---
+++ YStemAndChess/src/app/pages/student-profile/student-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-profile',
+  templateUrl: './student-profile.component.html',
+  styleUrls: ['./student-profile.component.scss']
+})
+export class StudentProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

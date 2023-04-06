cd27a79fd746db9e1ed88b4856626f26182d2ccc
Jeffrey Raia <44010413+Raia-Jeffrey@users.noreply.github.com>
2023-04-06 19:17:27 -0400

    Updated the typescript file
    same as title

--- YStemAndChess/src/app/pages/student-profile-2/student-profile-2.component1.ts
+++ YStemAndChess/src/app/pages/student-profile-2/student-profile-2.component1.ts
@@ -1,8 +1,65 @@
 //get canvas element
-const canvasElement = document.getElementById('myChart');
+const canvasElement = document.getElementById('myChart') as HTMLCanvasElement;
+
+//create interface for API Data retrieval
+interface Timecard {
+    username: string;
+    mentor: number;
+    lesson: number;
+    play: number;
+    puzzle: number;
+    website: number;
+}
+
+/*//date/times to paste in the URL to fetch from the API
+
+//const currentDate: Date = new Date();
+const username: string = "zteststudent";
+const startDate = new Date('2023-03-01T00:00:00');
+const endDate = new Date('2023-04-01T00:00:00');
+
+fetch("/timeTracking/statistics?username={zteststudent}&startDate=${2023-03-01T00:00:00}&endDate={2023-04-01T00:00:00.000Z}")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then(data => {
+    console.log(data);
+    timecardStorage.push(convertAPIValues(data));
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
+*/
+
+//-----------------------------------------------------
+const timecardStorage: any[] = [];
+
+let testAPIObj: Timecard = {
+    username: "zteststudent",
+    mentor: 368455,
+    lesson: 0,
+    play: 0,
+    puzzle: 0,
+    website: 0
+};
+
+function convertAPIValues(apiObj) {
+  let dummyAPIObj = apiObj;
+  let milMentor = dummyAPIObj.mentor / 60000;
+  let milLesson = dummyAPIObj.lesson / 60000;
+  let milPlay = dummyAPIObj.play / 60000;
+  let milPuzzle = dummyAPIObj.puzzle / 60000;
+  let milWebsite = dummyAPIObj.website / 60000;
+
+  let trueArray = [milWebsite, milLesson, milPuzzle, milPlay, milMentor];
+  return trueArray;
+};
 
 //dummy tables
-let monthX = [Math.floor(Math.random() * 20) + 1, Math.floor(Math.random() * 20) + 1, Math.floor(Math.random() * 20) + 1, Math.floor(Math.random() * 20) + 1, Math.floor(Math.random() * 20) + 1];
+timecardStorage.push(convertAPIValues(testAPIObj));
+let monthX = timecardStorage[0];
 let monthY = [Math.floor(Math.random() * 20) + 1, Math.floor(Math.random() * 20) + 1, Math.floor(Math.random() * 20) + 1, Math.floor(Math.random() * 20) + 1, Math.floor(Math.random() * 20) + 1];
 
 //chartjs config
@@ -60,13 +117,5 @@ let config = {
           borderRadius: 3
       },
       };
-
-let newChart = new Chart(canvasElement, config);
-          },
-          barPercentage: 0.5,
-          categoryPercentage: 1,
-          borderRadius: 3
-      },
-      };
-
-let newChart = new Chart(canvasElement, config);
+//@ts-ignore
+let newChart = new Chart(canvasElement, config), any;

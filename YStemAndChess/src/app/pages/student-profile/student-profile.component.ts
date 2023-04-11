ab9dec67efc5942dbff0ab18dc3901526e302128
Inaya Alkhatib <inayahill@gmail.com>
2023-04-11 18:10:59 -0400

    Update student-profile.component.ts

--- YStemAndChess/src/app/pages/student-profile/student-profile.component.ts
+++ YStemAndChess/src/app/pages/student-profile/student-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-student-profile',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private cookie: CookieService) { }
 
   ngOnInit(): void {
+   let userContent;
+   if (this.cookie.check('login')){
+    userContent = JSON.parse(atob(this.cookie.get('login').split('.')[1]));
+    this.httpGetAsync(
+    `GET`,
+    `${environment.urls.middlewareURL}/timeTracking/statistics?username={zteststudent}&startDate=\${2023-03-01T00:00:00}&endDate={2023-04-01T00:00:00.000Z}`,
+      (response) => {
+        if (
+          JSON.parse(response) ===
+          'There are no current meetings with this user.'
+        ) {
+          return;
+        }
+        let responseText = JSON.parse(response)[0];
+      }
+    );
+   } 
   }
 
 }

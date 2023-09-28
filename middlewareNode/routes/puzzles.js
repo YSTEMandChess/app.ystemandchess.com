00abfa1ce1f9f45bd6f57cc5fab8367d35834dc2
Samson Zhang <sz7651@rit.edu>
2023-09-28 13:56:59 -0400

    Added routes for three stages of game

--- middlewareNode/routes/puzzles.js
+++ middlewareNode/routes/puzzles.js
@@ -5,12 +5,50 @@ const puzzles = require('../models/puzzles')
 // Get all puzzles
 router.get('/list', async (req, res) => {
   try {
-      const puzzlesArray = await puzzles.find({}, {_id: 0 });
-      res.status(200).json(puzzlesArray);
-  } catch (error) {
-    console.error(error.message)
-    res.status(500).json('Server error')
+    const puzzlesArray = await puzzles.find({}, {_id: 0}).lean();
+    res.status(200).json(puzzlesArray)
+    console.log('All puzzles loaded')
+  } 
+  catch (error) {
+    console.error(error.message);
+    res.status(500).json('Server error');
   }
-})
+});
+
+// get opening puzzles
+router.get('/opening', async (req, res) => {
+  try {
+    const puzzlesArray = await puzzles.find({"Themes": {"$regex": "opening"}}, {_id: 0}).lean();
+    res.status(200).json(puzzlesArray);
+  } 
+  catch (error) {
+    console.error(error.message);
+    res.status(500).json('Server error');
+  }
+});
+
+// get middlegame puzzles
+router.get('/middlegame', async (req, res) => {
+  try {
+    const puzzlesArray = await puzzles.find({"Themes": {"$regex": "middlegame"}}, {_id: 0}).lean();
+    res.status(200).json(puzzlesArray);
+  } 
+  catch (error) {
+    console.error(error.message);
+    res.status(500).json('Server error');
+  }
+});
+
+// get endgame puzzles
+router.get('/endgame', async (req, res) => {
+  try {
+    const puzzlesArray = await puzzles.find({"Themes": {"$regex": "endgame"}}, {_id: 0}).lean();
+    res.status(200).json(puzzlesArray);
+  } 
+  catch (error) {
+    console.error(error.message);
+    res.status(500).json('Server error');
+  }
+});
 
 module.exports = router

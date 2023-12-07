efc49443997bde70a4f561ad9b1dd1ab8554e68a
Samson Zhang <sz7651@rit.edu>
2023-12-07 14:26:55 -0500

    Added text boxes and date checking

--- YStemAndChess/src/app/pages/gamification/streakprogress/streakprogress.component.ts
+++ YStemAndChess/src/app/pages/gamification/streakprogress/streakprogress.component.ts
@@ -14,6 +14,7 @@ export class StreakprogressComponent implements OnInit {
   streakNum = 9;
   todayDate = '12/07';
   weeklyProgressImages = ['FilledPiece', 'FilledPiece', 'FilledPiece', 'FilledPiece', 'TodayPiece', 'EmptyPiece', 'EmptyPiece'];
+  stemmyText = 'StemmyText1';
 
   // we import the header component because it contains user info
   constructor(private router: Router, private header: HeaderComponent) { }
@@ -45,17 +46,43 @@ export class StreakprogressComponent implements OnInit {
       }  
     });
 
+    // check today, if it is wednesday or later, change stemmy's text bubble
+    let currentDate = new Date();
+    if (currentDate.getDay() > 2 && currentDate.getDay() < 7){
+      this.stemmyText = 'StemmyText2';
+    }
+
+    // update the date
+    let month = currentDate.getMonth() + 1; // month starts at 0
+    let date = currentDate.getDate();
+    if (month < 10) {
+      this.todayDate = "0" + month + "/";
+    }
+    else {
+      this.todayDate = "" + month + "/";
+    }
+    if (date < 10) {
+      this.todayDate += "0" + date;
+    }
+    else {
+      this.todayDate += date;
+    }
+    
+
     // set up button and overlay
     const overlay = document.getElementById('overlay') as HTMLElement;
     const button = document.getElementById('stemmette') as HTMLElement;
+    const text = document.getElementById('stemmette-text') as HTMLElement;
     button.addEventListener('click', () => {
       console.log('<--  button clicked -->')
       overlay.style.display = 'flex';
       button.style.display = 'none';
+      text.style.display = 'none';
     })
     overlay.addEventListener('click', () => {
       overlay.style.display = 'none';
       button.style.display = 'block';
+      text.style.display = 'block';
     })
 
   }
